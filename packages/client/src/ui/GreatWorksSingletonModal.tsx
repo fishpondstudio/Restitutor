b568d62aa0c6ea87684fa92e05a02b9f8c8466fc
insraq <ruoysun@gmail.com>
2026-09-05 22:16:22 +0300

    Update Great Works UI

--- packages/client/src/ui/GreatWorksSingletonModal.tsx
+++ packages/client/src/ui/GreatWorksSingletonModal.tsx
@@ -1,23 +1,22 @@
 import { entriesOf } from "@project/shared/src/utils/Helper";
-import { Fragment } from "react/jsx-runtime";
 import { GreatWork } from "../game/definitions/GreatWork";
 import { $t, L } from "../utils/i18n";
 import { ModalComp, ModalTitleBar } from "../utils/ModalManager";
 import { GreatWorkComponent } from "./GreatWorkComponent";
+import { Grid2 } from "./UIConstant";
 
 export function GreatWorksSingletonModal(): React.ReactNode {
    return (
-      <ModalComp size="md" title={<ModalTitleBar title={$t(L.GreatWorks)} dismiss />}>
-         {entriesOf(GreatWork)
-            .sort((a, b) => a[1].completionYear - b[1].completionYear)
-            .map(([gw, config], idx) => (
-               <Fragment key={gw}>
-                  {idx > 0 && <div className="divider" />}
-                  <div className="m10">
+      <ModalComp size="lg" title={<ModalTitleBar title={$t(L.GreatWorks)} dismiss />}>
+         <div style={Grid2} className="m10">
+            {entriesOf(GreatWork)
+               .sort((a, b) => a[1].completionYear - b[1].completionYear)
+               .map(([gw, config], idx) => (
+                  <div className="box p10" key={gw}>
                      <GreatWorkComponent greatWork={gw} />
                   </div>
-               </Fragment>
-            ))}
+               ))}
+         </div>
       </ModalComp>
    );
 }
