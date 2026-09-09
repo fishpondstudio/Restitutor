f8730646af214c5e8accfff491d5008fa2128045
insraq <ruoysun@gmail.com>
2026-09-09 23:21:03 +0300

    Style tweak

--- packages/client/src/ui/SettleTilePage.tsx
+++ packages/client/src/ui/SettleTilePage.tsx
@@ -23,17 +23,15 @@ export function SettleTilePage({ tile }: { tile: Tile }): React.ReactNode {
       <SidebarComp
          title={<SidebarImageHeader image={Terrains[getTileTerrain(tile)].image} title={getTileName(tile, G.save)} />}
       >
-         <div className="box m10">
-            <div className="h1">{$t(L.TheFollowingConditionsMustBeMet)}</div>
-            {action.condition && <ConditionBreakdownComp condition={action.condition} />}
-            <div className="h1">{$t(L.TheFollowingResourcesWillBeSpent)}</div>
-            {action.cost && <ResourceCostComp cost={action.cost} />}
-            <div className="h1">{$t(L.TheFollowingEffectsWillBeApplied)}</div>
-            <div className="mx10 my5">{$t(L.$1LandTaxTileOutputAndManpowerFor$2Years, "-20%", "5")}</div>
-            <div className="mx10 my5">{$t(L.$1LandTaxTileOutputAndManpowerFor$2Years, "-20%", "10")}</div>
-            <div className="mx10 my5">{$t(L.$1LandTaxTileOutputAndManpowerFor$2Years, "-20%", "15")}</div>
-            <div className="mx10 my5">{$t(L.$1LandTaxTileOutputAndManpowerFor$2Years, "-20%", "20")}</div>
-         </div>
+         <div className="h1">{$t(L.TheFollowingConditionsMustBeMet)}</div>
+         {action.condition && <ConditionBreakdownComp condition={action.condition} />}
+         <div className="h1">{$t(L.TheFollowingResourcesWillBeSpent)}</div>
+         {action.cost && <ResourceCostComp cost={action.cost} />}
+         <div className="h1">{$t(L.TheFollowingEffectsWillBeApplied)}</div>
+         <div className="mx10 my5">{$t(L.$1LandTaxTileOutputAndManpowerFor$2Years, "-20%", "5")}</div>
+         <div className="mx10 my5">{$t(L.$1LandTaxTileOutputAndManpowerFor$2Years, "-20%", "10")}</div>
+         <div className="mx10 my5">{$t(L.$1LandTaxTileOutputAndManpowerFor$2Years, "-20%", "15")}</div>
+         <div className="mx10 my5">{$t(L.$1LandTaxTileOutputAndManpowerFor$2Years, "-20%", "20")}</div>
          <div className="m10">
             <ActionButton
                className="w100 py2"
